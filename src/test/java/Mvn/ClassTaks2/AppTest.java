94c55889e4f0301cd5cbbbd445c9fd22b23ad7c1
shaon <lmaknun@yahoo.com>
2024-02-17 09:01:46 -0800

    Maven project

---
+++ src/test/java/Mvn/ClassTaks2/AppTest.java
@@ -0,0 +1,17 @@
+package Mvn.ClassTaks2;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+
+public class AppTest 
+{
+    
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+       // assertTrue( true );
+    	System.out.println("Hello Everyone!");
+    }
+}
